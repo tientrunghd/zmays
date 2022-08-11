40c6ff351fa6482d3086d96092c6731cb88439e9
Rafah <101230324+rafahsk8@users.noreply.github.com>
2022-08-11 19:46:04 -0300

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,3 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
-& {$P = $env:TEMP + '\Sigil-1.9.10-Windows-x64-Setup.exe'; Invoke-WebRequest 'https://github.com/Sigil-Ebook/Sigil/releases/download/1.9.10/Sigil-1.9.10-Windows-x64-Setup.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
